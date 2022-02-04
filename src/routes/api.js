12058fab6b4fff368021cd13e320e1f5c4ba176f
HardikMirg <hardikmirgssv@gmail.com>
2022-02-04 10:37:44 +0530

    Connected to database, configured api, working tree clean!

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,8 +1,52 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+require('dotenv').config()
 
-router.get('/', (req, res) => {
-    res.send('Welcome to /api')
+router.get("/", (req, res) => {
+  const { google } = require("googleapis");
+  const spreadsheetId = process.env.SPREADSHEET_ID;
+
+  const getdata = async () => {
+    const auth = new google.auth.GoogleAuth({
+      keyFile: "./credentials.json",
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
+    });
+
+    const client = await auth.getClient();
+
+    const googleSheets = google.sheets({ version: "v4", auth: client });
+
+    const metaData = await googleSheets.spreadsheets.values.get({
+      auth,
+      spreadsheetId,
+      range: "jokes",
+    });
+
+    let data = metaData.data.values;
+
+    let jokify = (name, end) => {
+        let joke = {
+            0: "Knock, Knock!",
+            1: "Who's There?",
+            2: name,
+            3: `${name} who?`,
+            4: end
+        }
+        return joke;
+    }
+
+    let jokes = [];
+    data.forEach((item, index) => {
+    
+        let joke = jokify(data[index][2], data[index][4])
+        jokes.push(joke)
+    })
+
+
+    res.json(jokes)
+  };
+
+  getdata() 
 });
 
 module.exports = router;
